fd75b3887a7cb5d5057d43a7f45ef2a8f45cabf6
Ridwan Ahmad Maarif <ridwan.22693@gmail.com>
2019-09-07 21:57:24 +0700

    install

---
+++ index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Bhinestorm</title>
+  </head>
+  <body>
+<!-- Image and text -->
+<div class="top-bg"></div>
+<div class="ml-5 mt-3 mt-md-4 mb-5">
+      <a class="brandLogo" href="#"><img src="assets/group-109.svg"></a>
+</div>
+<div class="container margin-top">
+<div class="grid-bg"></div>
+<div class="rectangle-bg"></div>
+  <div class="row ml-0 mt-5 mb-5">
+    <div class="col ml-0 mt-5">
+      <img src="assets/group-108.svg" class="Group-108">
+    </div>
+    <div class="col mt-5">
+      <h1>Sorry, we are <br>under construction</h1>
+
+      <p class="content">I hope you’re not disappointed with this obstacle and we will finish this website soon. If you need help, please let me know!  </p>
+      <div class="text-center btn-position">
+        <button class="btn btn-primary rounded textButton" style="height: 49px;width: 291px;">Contact Us</button>
+      </div>  
+    </div>
+    
+    
+    
+  </div>
+          
+        
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
